85844b1affd99b6858a41a79241886c271e267a6
rioyi <wilbert.angarita@gmail.com>
2019-12-25 20:23:14 -0300

    init react from scratch

---
+++ index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const HelloComponent = () => {
+  return <h1>Hello Fucking World!!!</h1>
+}
+
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
